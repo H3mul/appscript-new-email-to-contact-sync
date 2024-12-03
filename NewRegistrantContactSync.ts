b7e0b3085a607a8ef7998eeebd5d32a66901a6c6
Phillip Dykman <phil.d324@gmail.com>
2024-12-03 16:59:04 -0400

    Implement email filtering and contact creation [7d4128]

--- NewRegistrantContactSync.ts
+++ NewRegistrantContactSync.ts
@@ -5,23 +5,124 @@
 // 2. Extract new registrant data from form in email body
 // 3. Create new Google Contact for registrant if doesn't exist
 
-function updateLastExecutionDate(): void {
+const RE_NEWLINE = "\\r\\n";
+
+function updateLastLookbackDate(date: Date): void {
     PropertiesService.getScriptProperties()
-        .setProperty(Settings.LAST_EXECUTION_DATE_PROP_NAME, new Date().toString());
+        .setProperty(Settings.LAST_LOOKBACK_DATE_PROP_NAME, date.toString());
 }
     
-function getLastExecutionDate(): Date {
+function getLastLookbackDate(): Date {
     const lastDateString = PropertiesService.getScriptProperties()
-                            .getProperty(Settings.LAST_EXECUTION_DATE_PROP_NAME);
+                            .getProperty(Settings.LAST_LOOKBACK_DATE_PROP_NAME);
     return lastDateString && new Date(lastDateString);
 }
 
-function getLookbackStart(lastTriggerDate: Date): Date {
+function getLookbackStart(): Date {
+    const lastDate = getLastLookbackDate();
     const maxLookback = new Date(new Date().setDate(new Date().getDate() - Settings.MAX_LOOKBACK_DAYS));
 
-    return lastTriggerDate < maxLookback ? maxLookback : lastTriggerDate;
+    return lastDate < maxLookback ? maxLookback : lastDate;
 }
 
+function getRequiredProp(propName: string) {
+    const value = PropertiesService.getScriptProperties().getProperty(propName);
+    if (!value) {
+        throw new Error(`Required script property not set: ${propName}`);
+    }
+    return value;
+}
+
+function getTargetEmailsAfter(start: Date): GoogleAppsScript.Gmail.GmailMessage[] {
+    const labelName = getRequiredProp(Settings.EMAIL_LABEL_PROP_NAME);
+    const targetSubject = new RegExp(getRequiredProp(Settings.EMAIL_SUBJECT_PROP_NAME),'i');
+    const label = GmailApp.getUserLabelByName(labelName);
+    
+    if (!label) {
+        throw new Error(`Label not found for name: ${labelName}`);
+    }
+
+    const targetMessages = [];
+
+    while (targetMessages.length < Settings.MAX_MESSAGE_PROCESSING_COUNT) {
+
+        // Only interested in first message of threads
+        const newMessages = label.getThreads(0, Settings.MAX_THREAD_COUNT_REQUEST)
+            .filter(t => t.getMessageCount() > 0)
+            .map(t => t.getMessages()[0]);
+
+        // Add new message matching subject and after start date
+        targetMessages.push(
+            ...newMessages.filter(m => m.getDate() > start && targetSubject.test(m.getSubject()))
+        );
+
+        if (!newMessages.length || newMessages[newMessages.length - 1].getDate() < start) {
+            break;
+        }
+    }
+    return targetMessages;
+}
+
+type Contact = {
+    givenName: string;
+    lastName: string;    
+    email: string;
+    phoneNumber: string;
+    address: string;
+    studentName?: string;
+}
+
+function extractFormField(form: string[], field: string, lines = 1): string {
+    const re = new RegExp(field, 'i');
+
+    let headingIndex = -1;
+    form.some((l, i) => {
+        if (re.test(l)) {
+            headingIndex = i;
+            return true;
+        }
+    });
+
+    if (headingIndex === -1 || form.length < headingIndex + 1 + lines) {
+        throw new Error(`Failed to extract form field: ${field}`);
+    }
+
+    return form.slice(headingIndex + 1, headingIndex + 1 + lines).join(' ').trim();
+}
+
+function extractContactFromMessage(message: GoogleAppsScript.Gmail.GmailMessage): Contact {
+    const formBody = message.getPlainBody().split('\n');
+
+    const [ givenName, lastName ] = extractFormField(formBody, "Your Name").split(' ');
+    return {
+        givenName, lastName,
+        email: extractFormField(formBody, "Email"),
+        phoneNumber: extractFormField(formBody, "Mobile Phone"),
+        address: extractFormField(formBody, "Address", 3),
+        studentName: extractFormField(formBody, "Student's First and Last Name"),
+    }
+}
+
+function createMissingContacts(contacts: Contact[]) {
+    const existingContacts = People.People.Connections.list('people/me', { personFields: 'emailAddresses' });
+
+    const newContacts = contacts.filter(c => 
+        existingContacts?.connections.some((connection) =>
+            connection.emailAddresses && connection.emailAddresses.some((emailAddress) => emailAddress.value === c.email)
+    ));
+
+    newContacts.forEach(c => {
+        People.People.createContact({
+            names: [{ givenName: c.givenName, familyName: c.lastName }],
+            emailAddresses: [{ value: c.email }],
+            phoneNumbers: [{ value: c.phoneNumber }],
+            addresses: [{ streetAddress: c.address }],
+            userDefined: [{ key: "Student's Name", value: c.studentName }]
+        })            
+    });
+
+    return newContacts;
+}
 
 function onTimeTrigger() {
     console.log(`Time trigger started`);
@@ -29,11 +130,21 @@ function onTimeTrigger() {
 }
 
 function onTrigger() {
-    const lastTriggerDate = getLastExecutionDate();
-    console.log(`Last execution timestamp: ${lastTriggerDate.toString()}`);
+    console.log(`Starting New Registrant Contact Sync script with properties:`);
+    console.log(`   ${JSON.stringify(PropertiesService.getScriptProperties().getProperties())}`);
 
-    const start = getLookbackStart(lastTriggerDate);
-    console.log(`Looking back at new registrants after: ${start.toString()}`);
+    const start = getLookbackStart();
+    console.log(`Looking back at new emails since: ${start.toString()}`);
 
+    const messages = getTargetEmailsAfter(start);
+    console.log(`Found ${messages.length} new target messages`);
 
+    if (messages.length) {
+        updateLastLookbackDate(new Date(messages[0].getDate().toString()))
+    }
+
+    const contacts = messages.map(m => extractContactFromMessage(m));
+    const createdContacts = createMissingContacts(contacts);
+
+    console.log(`Added ${createdContacts.length} new contacts ${createdContacts.map(c => c.email).join(', ')}`);
 }
--- Settings.ts
+++ Settings.ts
@@ -1,4 +1,10 @@
 namespace Settings {
-    export const LAST_EXECUTION_DATE_PROP_NAME = "last_execution";
+    export const LAST_LOOKBACK_DATE_PROP_NAME = "__last_lookback_date";
     export const MAX_LOOKBACK_DAYS = 30;
+
+    export const MAX_THREAD_COUNT_REQUEST = 10;
+    export const MAX_MESSAGE_PROCESSING_COUNT = 50;
+
+    export const EMAIL_LABEL_PROP_NAME = "EMAIL_LABEL";
+    export const EMAIL_SUBJECT_PROP_NAME = "EMAIL_SUBJECT";
 }
